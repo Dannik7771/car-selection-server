ad8de1626ab87a9fe49108fdf521a2573cbc3be6
Dannik7771 <taleforhelen@gmail.com>
2022-12-12 15:27:16 +0300

    Git init

---
+++ src/main/kotlin/com/ostdan/carselectionserver/CarSelectionServerApplication.kt
@@ -0,0 +1,11 @@
+package com.ostdan.carselectionserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CarSelectionServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<CarSelectionServerApplication>(*args)
+}
---
+++ src/test/kotlin/com/ostdan/carselectionserver/CarSelectionServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ostdan.carselectionserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CarSelectionServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
